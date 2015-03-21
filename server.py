f78bea5b78da59f46926c8d4da073c30927a9af8
Nic Donaldson <nic.donaldson.0@gmail.com>
2015-03-21 16:25:04 +1100

    barebones commit

---
+++ server.py
@@ -0,0 +1,27 @@
+import os
+import configparser
+import tornado.ioloop
+import tornado.web
+
+class Hello(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world!")
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+settings = {
+    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "cookie_secret":config['secrets']['cookie'],
+    "xsrf_cookies": True,
+}
+
+application = tornado.web.Application([
+    (r"/", Hello),
+    
+], **settings)
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
